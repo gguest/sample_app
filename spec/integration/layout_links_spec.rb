4422d3674b7ff48bc1dd1872f635f94ef6e4ed52
Greg Guest <gbguest@gmail.com>
2010-08-04 22:54:09 -0600

    Finished layout and routes

--- spec/integration/layout_links_spec.rb
+++ spec/integration/layout_links_spec.rb
@@ -20,6 +20,12 @@ describe "Layout Links" do
     get '/help'
     response.should render_template('pages/help')
   end
+    
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should render_template('users/new')
+ 
+  end
 end
 
 
